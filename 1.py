69a5f0b5599509e1236246d037a81e379572f84b
Babra Mpofu <134708569+BabraMpofu@users.noreply.github.com>
2025-04-21 20:03:21 +0200

    1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-#Babra Mpofu 45670544
+
 #this program ask the user to enter any negative integer number and use a for loop, to display output in such a way that the number is printed, and all subsequent ascending numbers, until 0 is reached
 number=int(input("enter a negative interger number: "))#ask the user to enter a number
 for i in range(number,1): #introducing a for loop condition
